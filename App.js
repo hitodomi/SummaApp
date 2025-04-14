85fa990afc161d3e829e089904149c19ca8ead3d
hitodomi <k23089kk@aitech.ac.jp>
2025-04-14 11:50:25 +0900

    Initial working version of SummaApp

--- App.js
+++ App.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import HomeScreen from './screens/HomeScreen';
-import CameraScreen from './screens/CameraScreen';
-import ResultScreen from './screens/ResultScreen';
+import HomeScreen from './app/HomeScreen';
+import CameraScreen from './app/CameraScreen';
+import ResultScreen from './app/ResultScreen';
+import { registerRootComponent } from 'expo'; // ← 追加！
 
 const Stack = createNativeStackNavigator();
 
-export default function App() {
+function App() {
     return (
         <NavigationContainer>
             <Stack.Navigator initialRouteName="Home" screenOptions={{ headerShown: false }}>
@@ -18,3 +19,6 @@ export default function App() {
         </NavigationContainer>
     );
 }
+
+// 👇 これで Expo が正しくAppを起動できるようになる！
+export default registerRootComponent(App);
